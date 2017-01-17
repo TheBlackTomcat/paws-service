67e4fb82115041828c2e32336d02de56f070f046
TheBlackTomcat <meow@theblacktomcat.com>
2017-01-18 00:53:31 +0200

    Added Entity ID and unique id generation support.

--- src/main/scala/paws/library/service/domain/Entity.scala
+++ src/main/scala/paws/library/service/domain/Entity.scala
@@ -1,6 +1,16 @@
 package paws.library.service.domain
 
 /**
-  * Marker trait for the Entity pattern as expressed by Domain Driven Design.
+  * Marker trait for the Entity pattern as expressed by Domain Driven Design. Contains an id of type EntityID.
   */
-trait Entity
+trait Entity {
+  def id: EntityID
+}
+
+/**
+  * EntityID ValueObject used as an unique id for entities.
+  * @param id The id of the Entity. Required.
+  */
+case class EntityID(id: String) {
+  require(id != null && !id.isEmpty)
+}
--- src/main/scala/paws/library/service/domain/Repository.scala
+++ src/main/scala/paws/library/service/domain/Repository.scala
@@ -1,6 +1,15 @@
 package paws.library.service.domain
 
+import java.util.UUID
+
 /**
   * Marker trait for the Repository pattern as expressed by Domain Driven Design.
   */
-trait Repository
+trait Repository {
+
+  /**
+    * Generates a unique random EntityID based on the Java UUID random generator.
+    * @return Unique EntityID.
+    */
+  def uniqueId(): EntityID = EntityID(UUID.randomUUID().toString.toUpperCase)
+}
