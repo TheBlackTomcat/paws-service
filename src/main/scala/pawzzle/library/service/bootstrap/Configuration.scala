1f37ac9ba6f9a3f487cf7b0d5e05d0ec23a80b73
TheBlackTomcat <meow@theblacktomcat.com>
2017-01-28 13:01:49 +0200

    Refactored to be easier to use. Transformed into objects.

--- src/main/scala/pawzzle/library/service/bootstrap/Configuration.scala
+++ src/main/scala/pawzzle/library/service/bootstrap/Configuration.scala
@@ -5,23 +5,24 @@ import scala.io.Source
 /**
   * Configuration object that loads properties from an service.properties file located in the classpath.
   */
-class Configuration {
-
-  private var cMap = Map.empty[String, String]
-
-  def getProp(name: String): String = cMap(name)
-
-  def getPropAsInt(name: String): Int = cMap(name).toInt
-}
-
 object Configuration {
 
-  def apply(): Configuration = {
+  private val filename = "service.properties"
+  private val map = Source.fromResource(filename).getLines().map(_.split("=")).map(a => a(0) -> a(1)).toMap
 
-    val filename: String = "service.properties"
-    val config = new Configuration()
+  /**
+    * Gets a string property.
+    *
+    * @param name The property name.
+    * @return The property value.
+    */
+  def getProp(name: String): String = map(name)
 
-    config.cMap = Source.fromResource(filename).getLines().map(_.split("=")).map(a => a(0) -> a(1)).toMap
-    config
-  }
+  /**
+    * Gets an integer property.
+    *
+    * @param name The property name.
+    * @return The property value.
+    */
+  def getPropAsInt(name: String): Int = map(name).toInt
 }
