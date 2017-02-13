dcfa6421e045aaf764758af6a8b8ea2513bbd535
TheBlackTomcat <meow@theblacktomcat.com>
2017-02-13 22:20:16 +0200

    Changed Adapter types.

--- src/main/scala/pawzzle/library/service/infrastructure/Adapter.scala
+++ src/main/scala/pawzzle/library/service/infrastructure/Adapter.scala
@@ -6,11 +6,24 @@ package pawzzle.library.service.infrastructure
 trait Adapter
 
 /**
-  * Marker trait for storage adapters, specialized type of Adapter.
+  * Marker trait for data access adapters, specialized type of Adapter.
   */
-trait StorageAdapter extends Adapter
+trait DataAccessAdapter extends Adapter
 
 /**
-  * Marker trait for integration adapters, specialized type of Adapter.
+  * Marker trait for RESTful web services adapters, specialized type of Adapter. RESTful WS adapter should define
+  * the API for RESTful WS Style Integrations as defined the the REST architectural style.
   */
-trait IntegrationAdapter extends Adapter
+trait RESTfulWSAdapter extends Adapter
+
+/**
+  * Marker trait for Messaging adapters, specialized type of Adapter. Messaging adapter should define the API for
+  * Message Style Integrations as defined by the Enterprise Integration Patterns.
+  */
+trait MessagingAdapter extends Adapter
+
+/**
+  * Marker trait for Notification adapters, specialized type of Adapter. Notification adapter should define the API
+  * for platform external notifications like email, sms etc.
+  */
+trait NotificationAdapter extends Adapter
